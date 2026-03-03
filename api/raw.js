ea88b9109b043f9c31304240e7308c4ad5ce80ff
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-03 18:25:48 +0200

    Update raw.js

--- api/raw.js
+++ api/raw.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
     if (host.includes("ethereos-auth")) { codeBranch = "auth"; fallbackFile = "getkey.html"; }
     else if (host.includes("ethereos-testing")) { codeBranch = "test"; fallbackFile = "test.html"; }
     else if (host.includes("ethereos-api")) { codeBranch = "api"; }
-    else if (host.includes("ethereos-api")) { codeBranch = "raw"; }
+    else if (host.includes("ethereos-raw")) { codeBranch = "raw"; }
     else if (host.includes("ethereos-cdn")) { codeBranch = "cdn"; }
     else if (host.includes("ethereos")) { codeBranch = "off"; }
 
