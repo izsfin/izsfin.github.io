3ac0f068883b0a8f769f8891034d37da9b8931f0
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-07 23:41:55 +0200

    Update

--- api/obfuscator.js
+++ api/obfuscator.js
@@ -292,7 +292,7 @@ ${invBlock3}
 ${junkBottom}
 end)(...)`;
 
-    const header = '--[[ V6 ZuraMO | ethereos.vercel.app/obfuscator ]]';
+    const header = '--[[ v2.14.6 NixuMO | ethereos.vercel.app/obfuscator ]]';
     return header + ' ' + minifyLua(lua);
 }
 
