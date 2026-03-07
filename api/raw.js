7c914a0f300b3ae0d0d5556965be54c8d17fbd87
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-07 23:43:31 +0200

    Update

--- api/raw.js
+++ api/raw.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
     else if (host.includes("ethereos-raw")) { codeBranch = "raw"; }
     else if (host.includes("ethereos-cdn")) { codeBranch = "cdn"; }
     else if (host.includes("ethereos")) { codeBranch = "off"; }
-
+    else if (host.includes("ethereos-testing")) { codeBranch = "test"; fallbackFile = "status.html"; }
 // --- 3.1. ИСКЛЮЧЕНИЯ ПО ПУТИ ---
     // --- 3.1. ИСКЛЮЧЕНИЯ ПО ПУТИ ---
     if (rawPath === "obfuscator") {
