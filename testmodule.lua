3231558ec721a494145b845466cf83f3288642bf
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-12 20:46:08 +0200

    Update

--- testmodule.lua
+++ testmodule.lua
@@ -1,15 +1,15 @@
 return {
-    metainfo = {
-        NameModule   = "TestModule",
-        VersionModule = "v1.0.0",
-        sversion     = "v1.0.0",
-        Author       = "mewix",
-        folder       = "testmodule",
-        callsyntax   = "tm",
-        csymbol      = "()",
-        Classes      = {"Outfit", "Accessory"},
-        socials      = {"Website : https://ethereos.vercel.app"},
-    },
+    -- metainfo прямо в корне
+    NameModule    = "TestModule",
+    VersionModule = "v1.0.0",
+    sversion      = "v1.0.0",
+    Author        = "mewix",
+    folder        = "testmodule",
+    callsyntax    = "tm",
+    csymbol       = "()",
+    Classes       = {"Outfit", "Accessory"},
+    socials       = {"Website : https://ethereos.vercel.app"},
+
     library = {
         {
             Class        = "Outfit",
@@ -23,13 +23,6 @@ return {
             Name         = "TestHat",
             FunctionCode = [[
                 addaccessory({ name = "testhat", mesh = "rbxassetid://73083430479187", texture = "rbxassetid://104381302798685", weld = "Head", pos = {0, 0.1, 0}, rt = {0, 0, 0} })
-            ]],
-        },
-        {
-            Class        = "secret",
-            Name         = "SecretItem",
-            FunctionCode = [[
-                print("secret item works!")
             ]],
         },
     }
