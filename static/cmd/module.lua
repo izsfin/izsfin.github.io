39284a5047ff4d1967f20cc4d08bdd0645651e45
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-24 19:32:23 +0200

    update

--- static/cmd/module.lua
+++ static/cmd/module.lua
@@ -1,14 +1,14 @@
 local Security = loadstring(http.request({
-    Url = "https://nekoq.vercel.app/static/cmd/security",
-    Headers = { ["User-Agent"] = "hux9z/software/cmd" }
+    Url = "https://nekoq.vercel.app/static/cmd/md/security",
+    Headers = { ["User-Agent"] = "hux9z/software" }
 }).Body)()
 
 local Helpers = loadstring(http.request({
-    Url = "https://nekoq.vercel.app/static/cmd/helpers",
-    Headers = { ["User-Agent"] = "hux9z/software/cmd" }
+    Url = "https://nekoq.vercel.app/static/cmd/md/helpers",
+    Headers = { ["User-Agent"] = "hux9z/software" }
 }).Body)()
 
 local ModuleSystem = loadstring(http.request({
-    Url = "https://nekoq.vercel.app/static/cmd/module",
-    Headers = { ["User-Agent"] = "hux9z/software/cmd" }
+    Url = "https://nekoq.vercel.app/static/cmd/md/module",
+    Headers = { ["User-Agent"] = "hux9z/software" }
 }).Body)()
