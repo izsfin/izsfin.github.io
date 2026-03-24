a1637093dcd676ee8ab2bbc7cb62ccdca2e6eae8
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-24 15:41:53 +0200

    update

--- xms.lua
+++ xms.lua
@@ -12,6 +12,7 @@
         Class = "Accessory",
         Subclass = "Head",
         Name = "White Waza Cat Beanie",
+        Weld = "Head",
         MeshID = "rbxassetid://17323590577",
         TextureID = "rbxassetid://17323590770",
         Scale = Vector3.new(0, -0.29, 0.1),
@@ -32,12 +33,14 @@
         Class = "Accessory",
         Subclass = "Face",
         Name = "White Tactical Ski Mask",
+        Weld = "Head",
         MeshID = "rbxassetid://111293653345754",
         TextureID = "rbxassetid://127827430442953",
         CFrame = CFrame.new(0,0.3,0) * CFrame.Angles(0, math.rad(275), 0)
         Scale = Vector3.new(1,1,1),
 		ReqPlaceID = 0
     },
+
 
     Balaclava Fisher Hat Black
     Name = "Glasses",
@@ -47,6 +50,18 @@
     Scale = "1,1,1",
     Offset = CFrame.new(0, -0.05, 0.04) * CFrame.fromEulerAnglesXYZ(math.rad(0), math.rad(0), math.rad(0))
 
+    {
+        Class = "Accessory",
+        Subclass = "Head",
+        Name = "Balaclava Fisher Hat Black",
+        Weld = "Head",
+        MeshID = "rbxassetid://79393866448461",
+        TextureID = "rbxassetid://72494023503362",
+        CFrame = CFrame.new(0, -0.05, 0.04) * CFrame.Angles(0, math.rad(0), 0)
+        Scale = Vector3.new(1,1,1),
+		ReqPlaceID = 0
+    },
+
     Triple Cross White Ushanka
     Name = "Glasses",
     MeshID = "rbxassetid://17897804310",
@@ -54,6 +69,18 @@
     WeldTo = "Head",
     Scale = "1,1,1",
     Offset = CFrame.new(0, -0.2, 0) * CFrame.fromEulerAnglesXYZ(math.rad(0), math.rad(0), math.rad(0))
+
+    {
+        Class = "Accessory",
+        Subclass = "Head",
+        Name = "Triple White Ushanka",
+        Weld = "Head",
+        MeshID = "rbxassetid://17897804310",
+        TextureID = "rbxassetid://17897850826",
+        CFrame = CFrame.new(0, -0.2, 0) * CFrame.Angles(0, math.rad(0), 0)
+        Scale = Vector3.new(1,1,1),
+		ReqPlaceID = 0
+    },
 
     Black Scarf Neck Mask (Low)
     Name = "Mask",
@@ -63,6 +90,17 @@
     Scale = "1,1,1",
     Offset = CFrame.new(0, 0.4, 0) * CFrame.fromEulerAnglesXYZ(math.rad(0), math.rad(0), math.rad(0))
 
+    {
+        Class = "Accessory",
+        Subclass = "Head",
+        Name = "Black Scarf Neck Mask (Low)",
+        MeshID = "rbxassetid://137131738580926",
+        TextureID = "rbxassetid://107504947928873",
+        CFrame = CFrame.new(0, 0.4, 0) * CFrame.Angles(0, math.rad(0), 0)
+        Scale = Vector3.new(1,1,1),
+		ReqPlaceID = 0
+    },
+
     white polarized tactical sport sunglasses
 	Name = "Glasses",
     MeshID = "rbxassetid://113646625498029",
@@ -71,6 +109,17 @@
     Scale = "1,0.9,1",
     Offset = CFrame.new(0.09, -0.19, 0) * CFrame.fromEulerAnglesXYZ(math.rad(0), math.rad(90), math.rad(0))
 
+    {
+        Class = "Accessory",
+        Subclass = "Face",
+        Name = "White Polarized Glasses",
+        MeshID = "rbxassetid://113646625498029",
+        TextureID = "rbxassetid://117520750964455",
+        CFrame = CFrame.new(0.09, -0.19, 0) * CFrame.Angles(0, math.rad(90), 0)
+        Scale = Vector3.new(1, 0.9, 1),
+		ReqPlaceID = 0
+    },
+
     pink fire polarized tactical sunglasses
     Name = "Glasses",
     MeshID = "rbxassetid://113646625498029",
@@ -78,6 +127,17 @@
     WeldTo = "Head",
     Scale = "1,0.9,1",
     Offset = CFrame.new(0.09, -0.19, 0) * CFrame.fromEulerAnglesXYZ(math.rad(0), math.rad(90), math.rad(0))
+ 
+    {
+        Class = "Accessory",
+        Subclass = "Face",
+        Name = "Pink Polarized Glasses",
+        MeshID = "rbxassetid://113646625498029",
+        TextureID = "rbxassetid://91791402195253",
+        CFrame = CFrame.new(0.09, -0.19, 0) * CFrame.Angles(0, math.rad(90), 0)
+        Scale = Vector3.new(1, 0.9, 1),
+        ReqPlaceID = 0
+    },
 
     Hair
     Name = "Black Punk Messy",
@@ -85,6 +145,17 @@
     TextureID = "rbxassetid://84167954362049",
     WeldTo = "Head",
     Offset = CFrame.new(0, -0.21, 0) * CFrame.fromEulerAnglesXYZ(math.rad(0), math.rad(180), math.rad(0))}
+
+    {
+        Class = "Accessory",
+        Subclass = "Head",
+        Name = "Black Punk Messy",
+        MeshID = "rbxassetid://91706780068690",
+        TextureID = "rbxassetid://84167954362049",
+        CFrame = CFrame.new(0, -0.21, 0) * CFrame.Angles(0, math.rad(180), 0),
+        Scale = Vector3.new(1,1,1),
+		ReqPlaceID = 0
+    },
 
     Name = "White Vest",
     WeldTo = "Torso",
