3ce7f0531235aee8b7c0b906f725dd124ebbdb85
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-24 23:46:22 +0200

    upd

--- base/s.ua.lua
+++ base/s.ua.lua
@@ -1,5 +1,5 @@
 -- [[ base/s.ua ]]
 local user_agent = "hux9z/software" -- Твой уникальный UA
-local base_url = "https://vellote.vercel.app/static" -- Ссылка на твой хост (обязательно со слэшем в конце)
+local base_url = "https://vellote.vercel.app/" -- Ссылка на твой хост (обязательно со слэшем в конце)
 
 return user_agent, base_url
