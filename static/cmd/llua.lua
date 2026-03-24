3fce926bf7db3cb0ce793d2928bc7c9c5f76632a
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-24 18:55:25 +0200

    update

--- static/cmd/llua.lua
+++ static/cmd/llua.lua
@@ -1,13 +1,1 @@
-return function(http)
-    local UA   = "hux9z/software"
-    local BASE = "https://nekoq.vercel.app/static/cmd/"
-    
-    return {
-        UA      = UA,
-        BASE    = BASE,
-        Library = loadstring(http.request({ Url = BASE .. "library", Headers = { ["User-Agent"] = UA } }).Body)(),
-        Meta    = loadstring(http.request({ Url = BASE .. "meta",    Headers = { ["User-Agent"] = UA } }).Body)(),
-        Logic   = loadstring(http.request({ Url = BASE .. "logic",   Headers = { ["User-Agent"] = UA } }).Body)(),
-        Module  = loadstring(http.request({ Url = BASE .. "module",  Headers = { ["User-Agent"] = UA } }).Body)()
-    }
-end
+return "hux9z/software", "https://nekoq.vercel.app/static/cmd"
