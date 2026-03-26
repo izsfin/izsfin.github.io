aa19eaa0ab41a90625205fa88c5f0c190e5bce59
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-26 20:47:45 +0200

    update

--- api/raw.js
+++ api/raw.js
@@ -1,7 +1,7 @@
 import { Octokit } from "@octokit/rest";
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-const OWNER = "varmoxd";
+const OWNER = "phxmale";
 const REPO = "wexly";
 
 export default async function handler(req, res) {
