a11899bbcebf1946ee1606147ce0a7a4b46e6715
nettoxi <bogdan.sumskoy@gmail.com>
2026-03-30 16:04:11 +0300

    update

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,4 +1,4 @@
-import { Octokit } from "@octokit/rest";
+import { Octokit } from "https://esm.sh/@octokit/rest";
 
 export async function onRequest(context) {
     const { request, env } = context;
@@ -241,6 +241,6 @@ async function sendToLogger(ip, path, domain, userAgent, status) {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ ip, path, domain, userAgent, status })
-        }); 
+        });
     } catch (e) {}
 }
