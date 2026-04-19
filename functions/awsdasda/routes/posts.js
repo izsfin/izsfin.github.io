4ebaba40c1db25dafa846f59043be9877111ab06
nettoxi <bogdan.sumskoy@gmail.com>
2026-04-19 14:25:19 +0300

    Update

--- functions/awsdasda/routes/posts.js
+++
@@ -1,6 +0,0 @@
-export async function handleGetPosts(env, headers) {
-    const { results } = await env.DB.prepare(
-        'SELECT id, name, "desc", img, author, date FROM posts ORDER BY date DESC'
-    ).all();
-    return new Response(JSON.stringify({ posts: results }), { headers });
-}
